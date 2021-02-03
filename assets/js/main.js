1fe6f34e9e4fc1e0d4e9c05e6c69bf78fa4436d2
VM <vmaksimovic.cr34@gmail.com>
2021-02-04 00:02:16 +0100

    adding login form and validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,27 @@
-// test JS link
-console.log("Test");
+// swap logo with login form
+const logoLink = document.getElementById("logo");
+const loginContainer = document.querySelector(".login-container");
+
+logoLink.addEventListener("click", () => {
+  logoLink.style.display = "none";
+  loginContainer.style.display = "block";
+});
+
+// login f-n and validation
+const loginBtn = document.getElementById("login-btn");
+
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  //vars
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  //validation
+  if (username === "user" && password === "pass") {
+    window.location = "./../../pages/info.html";
+    return false;
+  } else {
+    alert("Погрешно корисничко име или шифра.");
+  }
+});
+
