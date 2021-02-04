abea45a0b3c77e8082d441196e92703dd436a392
VM <vmaksimovic.cr34@gmail.com>
2021-02-04 18:11:37 +0100

    styling login form

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,7 @@ loginBtn.addEventListener("click", (e) => {
   //vars
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
+  const loginForm = document.getElementById("login-form");
 
   //validation
   if (username === "user" && password === "pass") {
@@ -22,6 +23,7 @@ loginBtn.addEventListener("click", (e) => {
     return false;
   } else {
     alert("Погрешно корисничко име или шифра.");
+    loginForm.reset();
   }
 });
 
