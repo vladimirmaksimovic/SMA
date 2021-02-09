114cea04f0b292346b246cd16c0ee7142155ce82
VM <vmaksimovic.cr34@gmail.com>
2021-02-09 22:21:14 +0100

    implementing search fn in address book

---
+++ assets/js/tableSearch.js
@@ -0,0 +1,20 @@
+function contactSearch() {
+  // vars
+  const input = document.getElementById("search-bar");
+  const filter = input.value.toUpperCase();
+  const table = document.getElementById("address-book");
+  const tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
