48d185a63f12d385c93ebcf947aeba75f91f50de
VM <vmaksimovic.cr34@gmail.com>
2021-02-10 23:56:40 +0100

    adding content

--- assets/js/tableSearch.js
+++ assets/js/tableSearch.js
@@ -1,3 +1,4 @@
+// pretraga kontakata
 function contactSearch() {
   // vars
   const input = document.getElementById("search-bar");
@@ -8,6 +9,29 @@ function contactSearch() {
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+// pretraga konta po opisu
+function accountSearch() {
+  // vars
+  const input = document.getElementById("search-bar");
+  const filter = input.value.toUpperCase();
+  const table = document.getElementById("account-book");
+  const tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    // change to [0] for search by account number
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
