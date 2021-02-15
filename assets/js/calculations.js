fe11a36680890b77159b50e43d6c3a8c94289794
VM <vmaksimovic.cr34@gmail.com>
2021-02-15 15:39:45 +0100

    adding logic in calculations page

--- assets/js/calculations.js
+++ assets/js/calculations.js
@@ -1,24 +1,113 @@
-/* const sumBtn = document.getElementById("threeMonthCalc");
-console.log(inputFields[1].value);
-
-sumBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  const inputFields = document.getElementsByClassName("calc-input");
-  for (let i = 0; i < inputFields.length; i++) {
-    let sum = 0;
-    inputFields.value += inputFields[i].value;
-    console.log(inputFields);
-  }
-}) */
+// vars
+/* let totalMonth = document.querySelectorAll(".calc-total-output");
+let averageMonth = document.querySelectorAll(".calc-average-output"); */
+const calcThreeMonth = document.getElementById("threeMonthCalc");
+const calcSixMonth = document.getElementById("sixMonthCalc");
+const calcTwelveMonth = document.getElementById("twelveMonthCalc");
+//const clearInputFields = document.getElementById("btn-clear");
+const clearInputFields = document.querySelectorAll(".clear-btn");
+const calcCreditLimit = document.getElementById("creditLimitCalc");
+/* const calcBtn = document.querySelectorAll(".calc-btn");
+console.log(calcBtn); */
 
 // 3 month average
-const threeMonthCalc = document.getElementById("threeMonthCalc");
-const clearInputFields = document.getElementById("btn-clear");
+calcThreeMonth.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const inputFields = document.querySelectorAll(".calc-input-three");
+  let totalCalc = document.getElementById("total-three-month-calc");
+  let averageCalc = document.getElementById("average-three-month-calc");
+
+  let sum = 0;
+  let average;
+
+  for (let i = 0; i < inputFields.length; i++) {
+    let value = parseFloat(inputFields[i].value);
+    sum = sum + value;
+    average = sum / i;
+  }
+
+  //console.log(totalCalc);
+  //console.log(averageCalc);
+  //console.log(`Sum = ${sum}, Average = ${average}`);
+  totalCalc.value = sum;
+  averageCalc.value = average;
+
+  //console.log(totalMonth);
+  /* for (let i = 0; i < totalMonth.length; i++) {
+    totalMonth[i].value = sum;
+    averageMonth[i].value = average;
+  } */
+});
+
+// 6 month average
+calcSixMonth.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const inputFields = document.querySelectorAll(".calc-input-six");
+  let totalCalc = document.getElementById("total-six-month-calc");
+  let averageCalc = document.getElementById("average-six-month-calc");
+
+  let sum = 0;
+  let average;
+
+  for (let i = 0; i < inputFields.length; i++) {
+    let value = parseFloat(inputFields[i].value);
+    sum = sum + value;
+    average = sum / i;
+  }
+
+  console.log(totalCalc);
+  console.log(averageCalc);
+  console.log(`Sum = ${sum}, Average = ${average}`);
+  totalCalc.value = sum;
+  averageCalc.value = average;
+
+  //console.log(totalMonth);
+  /* for (let i = 0; i < totalMonth.length; i++) {
+    totalMonth[i].value = sum;
+    averageMonth[i].value = average;
+  } */
+});
+
+// 12 month average
+calcTwelveMonth.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const inputFields = document.querySelectorAll(".calc-input-twelve");
+  let totalCalc = document.getElementById("total-twelve-month-calc");
+  let averageCalc = document.getElementById("average-twelve-month-calc");
+
+  let sum = 0;
+  let average;
+
+  for (let i = 0; i < inputFields.length; i++) {
+    let value = parseFloat(inputFields[i].value);
+    sum = sum + value;
+    average = sum / i;
+  }
+
+  console.log(totalCalc);
+  console.log(averageCalc);
+  console.log(`Sum = ${sum}, Average = ${average}`);
+  totalCalc.value = sum;
+  averageCalc.value = average;
+
+  //console.log(totalMonth);
+  /* for (let i = 0; i < totalMonth.length; i++) {
+    totalMonth[i].value = sum;
+    averageMonth[i].value = average;
+  } */
+});
+
+
+// 3 month average
+/* const threeMonthCalc = document.getElementById("threeMonthCalc");
 
 threeMonthCalc.addEventListener("click", (event) => {
   event.preventDefault();
 
-  // get inputs
+  get inputs
   const firstMonth = parseFloat(document.getElementById("first-month").value);
 
   const secondMonth = parseFloat(document.getElementById("second-month").value);
@@ -26,28 +115,47 @@ threeMonthCalc.addEventListener("click", (event) => {
   const totalMonth = document.getElementById("total-month");
   const averageMonth = document.getElementById("average-month");
 
-  // total
+  total
   const total = firstMonth + secondMonth + thirdMonth;
   totalMonth.value = parseFloat(total);
 
-  // average
+  average
   const average = total / 3;
   averageMonth.value = average
-})
+}) */
 
 // clear fields
-clearInputFields.addEventListener("click", (event) => {
+for (let j = 0; j < clearInputFields.length; j++) {
+  clearInputFields[j].addEventListener("click", (event) => {
+    event.preventDefault();
+    const inputArray = document.getElementsByTagName("input");
+
+    for (let i = 0; i < inputArray.length; i++) {
+      inputArray[i].value = '';
+    }
+  });
+}
+
+
+// credit limit
+calcCreditLimit.addEventListener("click", (event) => {
   event.preventDefault();
 
-  const inputArray = document.getElementsByTagName("input");
-  console.log(inputArray[0]);
+  const netSalary = document.getElementById("net-salary");
+  const suspensions = document.getElementById("suspensions");
+  let thirdSalary = document.getElementById("one-third-salary");
+  let halfSalary = document.getElementById("one-half-salary");
+  const thirdSalaryLimit = document.getElementById("one-third-limit");
+  const halfSalaryLimit = document.getElementById("one-half-limit");
 
-  for (let i = 0; i < inputArray.length; i++) {
-    inputArray[i].value = '';
-  }
-})
+  third = parseFloat(netSalary.value) / 3;
+  half = parseFloat(netSalary.value) / 2;
 
-// 6 month average
+  const thirdLimit = third - parseFloat(suspensions.value);
+  const halfLimit = half - parseFloat(suspensions.value);
 
-
-// 12 month average
+  thirdSalary.value = third;
+  halfSalary.value = half
+  thirdSalaryLimit.value = thirdLimit;
+  halfSalaryLimit.value = halfLimit;
+});
