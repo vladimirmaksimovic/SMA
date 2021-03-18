896b3263dc84691f582c1ee2cf9b3395790904f5
SM <svetmar.office@gmail.com>
2021-03-18 09:38:40 +0100

    adding data

---
+++ assets/data/infoData.js
@@ -0,0 +1,21 @@
+// navbar data
+const navbar = {
+  info: "#",
+  requisition: "./requisition.html",
+  calculations: "./calculations.html",
+  addressBook: "./address-book.html",
+  accountPlan: "./account-plan.html",
+  links: [
+    { crf: "https://crf.trezor.gov.rs/" },
+    { croso: "https://www.croso.gov.rs/cir/index.php" },
+    { trezor: "https://www.trezor.gov.rs/src/" },
+    { poreskaUprava: "https://www.purs.gov.rs/" },
+    { eUprava: "https://euprava.gov.rs/" },
+    { gradKraljevo: "https://www.kraljevo.rs/" },
+    { nbs: "https://nbs.rs/sr_RS/indeks/" },
+    { vmCommerce: "https://vmcommerce.rs/" },
+    { maxPro: "https://maxpro.rs/" },
+    { school: "https://ossvetozarmarkovic.wordpress.com/" },
+  ],
+  signOut: "./../index.html",
+};
