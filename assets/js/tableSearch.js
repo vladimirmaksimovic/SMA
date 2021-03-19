bfa8b7d180b3753392e409a908f233804d8ee3a4
SM <svetmar.office@gmail.com>
2021-03-19 08:42:10 +0100

    adding account serach options

--- assets/js/tableSearch.js
+++ assets/js/tableSearch.js
@@ -1,4 +1,4 @@
-// pretraga kontakata
+// contact search
 function contactSearch() {
   // vars
   const input = document.getElementById("search-bar");
@@ -20,7 +20,7 @@ function contactSearch() {
   }
 }
 
-// pretraga konta po opisu
+// search by account name
 function accountSearch() {
   // vars
   const input = document.getElementById("search-bar");
@@ -32,6 +32,52 @@ function accountSearch() {
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[1];
     // change to [0] for search by account number
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+// search by account
+function accountNameSearch() {
+  // vars
+  const input = document.getElementById("search-bar-name");
+  const filter = input.value.toUpperCase();
+  const table = document.getElementById("account-plan");
+  const tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    // [0] for search by account number, [1] for search by account name
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+// search by account number
+function accountNumberSearch() {
+  // vars
+  const input = document.getElementById("search-bar-number");
+  const filter = input.value.toUpperCase();
+  const table = document.getElementById("account-plan");
+  const tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    // [0] for search by account number, [1] for search by account name
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
