db84da879e80ee134dd8863dc80b8142b8f164aa
VM <vmaksimovic.cr34@gmail.com>
2021-04-13 23:36:03 +0200

    school bell data

--- view/pages/info.php
+++ view/pages/info.php
@@ -5,6 +5,7 @@ include_once './../../data/data.php';
 
 $divisions_test = isset($GLOBALS["divisions_test"]) ? $GLOBALS["divisions_test"] : "";
 $suppliers = isset($GLOBALS["suppliers"]) ? $GLOBALS["suppliers"] : "";
+$school_bell = isset($GLOBALS["school_bell"]) ? $GLOBALS["school_bell"] : "";
 
 ?>
 
@@ -90,7 +91,8 @@ $suppliers = isset($GLOBALS["suppliers"]) ? $GLOBALS["suppliers"] : "";
           <tr>
             <td>Website</td>
             <td class="data-container">
-              <a href="https://ossvetozarmarkovic.wordpress.com/" target="_blank">https://ossvetozarmarkovic.wordpress.com/</a>
+              <a href="https://ossvetozarmarkovic.wordpress.com/"
+                target="_blank">https://ossvetozarmarkovic.wordpress.com/</a>
             </td>
           </tr>
           <tr>
@@ -128,16 +130,16 @@ $suppliers = isset($GLOBALS["suppliers"]) ? $GLOBALS["suppliers"] : "";
       <h2 id="division-heading">Одељењске старешине</h2>
       <div class="division-container">
         <?php foreach ($divisions_test as $division_title => $divisions) { ?>
-          <div class="division first-div">
-            <h3><?php echo $division_title; ?></h3>
-            <ul>
-              <?php foreach ($divisions as $division_name => $division_teacher) { ?>
-                <li>
-                  <strong><?php echo $division_name; ?></strong> <?php echo $division_teacher; ?>
-                </li>
-              <?php } ?>
-            </ul>
-          </div>
+        <div class="division first-div">
+          <h3><?php echo $division_title; ?></h3>
+          <ul>
+            <?php foreach ($divisions as $division_name => $division_teacher) { ?>
+            <li>
+              <strong><?php echo $division_name; ?></strong> <?php echo $division_teacher; ?>
+            </li>
+            <?php } ?>
+          </ul>
+        </div>
         <?php } ?>
       </div>
     </article>
@@ -145,192 +147,34 @@ $suppliers = isset($GLOBALS["suppliers"]) ? $GLOBALS["suppliers"] : "";
 
     <!-- school bell -->
     <article class="school-bell">
-      <?php foreach ($school_bell as $school_bell_heading => $shifts) { ?>
-        <h2 id="school-bell-heading"><?php $school_bell_heading ?></h2>
-        <div class="shift-container">
-          <?php foreach ($shifts as $shift_heading => $shift_data) { ?>
-            <table class="first-shift shift">
-              <thead>
-                <tr>
-                  <th colspan="2"><?php echo $shift_heading; ?></th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php foreach ($shift_data as $classes_order => $classes_time) { ?>
-                  <tr>
-                    <td><?php echo $classes_order; ?></td>
-                    <td><?php echo $classes_order; ?></td>
-                  </tr>
-                <?php } ?>
-              </tbody>
-            </table>
-          <?php } ?>
-        </div>
-      <?php } ?>
-      <!-- 
-      <h2 id="school-bell-heading">Распоред звоњења - редовна настава</h2>
-      <div class="shift-container">
-        <table class="first-shift shift">
-          <thead>
-            <tr>
-              <th colspan="2">Прва смена</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td colspan="2">07:50 - улазак ученика</td>
-            </tr>
-            <tr>
-              <td>1. час</td>
-              <td>08:00 - 08:45</td>
-            </tr>
-            <tr>
-              <td>2. час</td>
-              <td>08:50 - 09:35</td>
-            </tr>
-            <tr>
-              <td colspan="2">10:00 - улазак ученика</td>
-            </tr>
-            <tr>
-              <td>3. час</td>
-              <td>10:05 - 10:50</td>
-            </tr>
-            <tr>
-              <td>4. час</td>
-              <td>10:55 - 11:40</td>
-            </tr>
-            <tr>
-              <td>5. час</td>
-              <td>11:45 - 12:30</td>
-            </tr>
-            <tr>
-              <td>6. час</td>
-              <td>12:35 - 13:20</td>
-            </tr>
-          </tbody>
-        </table>
-        <table class="second-shift shift">
-          <thead>
-            <tr>
-              <th colspan="2">Друга смена</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td colspan="2">13:20 - улазак ученика</td>
-            </tr>
-            <tr>
-              <td>1. час</td>
-              <td>13:30 - 14:15</td>
-            </tr>
-            <tr>
-              <td>2. час</td>
-              <td>14:20 - 15:05</td>
-            </tr>
-            <tr>
-              <td colspan="2">15:30 - улазак ученика</td>
-            </tr>
-            <tr>
-              <td>3. час</td>
-              <td>15:35 - 16:20</td>
-            </tr>
-            <tr>
-              <td>4. час</td>
-              <td>16:25 - 17:10</td>
-            </tr>
-            <tr>
-              <td>5. час</td>
-              <td>17:15 - 18:00</td>
-            </tr>
-            <tr>
-              <td>6. час</td>
-              <td>18:05 - 18:50</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
 
-      <h2 id="school-bell-heading">Распоред звоњења - скраћени часови</h2>
+      <?php foreach ($school_bell as $school_bell_heading => $shifts) { ?>
+      <h2 id="school-bell-heading"><?php $school_bell_heading; ?></h2>
       <div class="shift-container">
-        <table class="first-shift shift">
+        <?php foreach ($shifts as $shift_heading => $shift_data) { ?>
+        <table class="shift">
           <thead>
             <tr>
-              <th colspan="2">Прва смена</th>
+              <th colspan="2"><?php echo $shift_heading; ?></th>
             </tr>
           </thead>
+          <?php foreach ($shift_data as $classes_order => $classes_time) { ?>
           <tbody>
+
             <tr>
-              <td colspan="2">07:50 - улазак ученика</td>
+              <td><?php echo $classes_order; ?></td>
+              <td><?php echo $classes_time; ?></td>
             </tr>
-            <tr>
-              <td>1. час</td>
-              <td>08:00 - 08:45</td>
-            </tr>
-            <tr>
-              <td>2. час</td>
-              <td>08:50 - 09:35</td>
-            </tr>
-            <tr>
-              <td colspan="2">10:00 - улазак ученика</td>
-            </tr>
-            <tr>
-              <td>3. час</td>
-              <td>10:05 - 10:50</td>
-            </tr>
-            <tr>
-              <td>4. час</td>
-              <td>10:55 - 11:40</td>
-            </tr>
-            <tr>
-              <td>5. час</td>
-              <td>11:45 - 12:30</td>
-            </tr>
-            <tr>
-              <td>6. час</td>
-              <td>12:35 - 13:20</td>
-            </tr>
+
           </tbody>
+          <?php } ?>
         </table>
-        <table class="second-shift shift">
-          <thead>
-            <tr>
-              <th colspan="2">Друга смена</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td colspan="2">13:20 - улазак ученика</td>
-            </tr>
-            <tr>
-              <td>1. час</td>
-              <td>13:30 - 14:15</td>
-            </tr>
-            <tr>
-              <td>2. час</td>
-              <td>14:20 - 15:05</td>
-            </tr>
-            <tr>
-              <td colspan="2">15:30 - улазак ученика</td>
-            </tr>
-            <tr>
-              <td>3. час</td>
-              <td>15:35 - 16:20</td>
-            </tr>
-            <tr>
-              <td>4. час</td>
-              <td>16:25 - 17:10</td>
-            </tr>
-            <tr>
-              <td>5. час</td>
-              <td>17:15 - 18:00</td>
-            </tr>
-            <tr>
-              <td>6. час</td>
-              <td>18:05 - 18:50</td>
-            </tr>
-          </tbody>
-        </table>
-      </div> -->
+        <?php } ?>
+      </div>
+      <?php } ?>
+
+      <pre><?php print_r($school_bell) ?></pre>
+
     </article>
     <!-- /school bell -->
 
@@ -349,12 +193,12 @@ $suppliers = isset($GLOBALS["suppliers"]) ? $GLOBALS["suppliers"] : "";
         <tbody>
 
           <?php foreach ($suppliers as list($supplier_name, $supplier_phone, $supplier_email, $supplier_contact)) { ?>
-            <tr>
-              <td><?php echo $supplier_name; ?></td>
-              <td><?php echo $supplier_phone; ?></td>
-              <td><?php echo $supplier_email; ?></td>
-              <td><?php echo $supplier_contact; ?></td>
-            </tr>
+          <tr>
+            <td><?php echo $supplier_name; ?></td>
+            <td><?php echo $supplier_phone; ?></td>
+            <td><?php echo $supplier_email; ?></td>
+            <td><?php echo $supplier_contact; ?></td>
+          </tr>
           <?php } ?>
         </tbody>
       </table>
