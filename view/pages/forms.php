b0fba64a44080b05ca4dea920c0f701bdf63f4ce
SM <svetmar.office@gmail.com>
2021-04-16 09:37:42 +0200

    adding content

---
+++ view/pages/forms.php
@@ -0,0 +1,54 @@
+<?php
+
+include_once './../components/header.php';
+
+?>
+
+<!-- main -->
+<main>
+    <!-- aside -->
+    <aside>
+        <nav class="aside-container">
+            <ul>
+                <li><a href="#form1">Образац 1</a></li>
+                <li><a href="#form2">Образац 2</a></li>
+                <li><a href="#form3">Образац 3</a></li>
+                <li><a href="#form4">Образац 4</a></li>
+                <li><a href="#"><i class="fas fa-arrow-up"></i> Врх стране</a></li>
+            </ul>
+        </nav>
+    </aside>
+    <!-- /aside -->
+
+    <!-- content  -->
+    <section class="content">
+        <!-- form 1 -->
+        <article class="forms">
+            <h2 id="form1">Образац 1</h2>
+        </article>
+        <!-- /form 1 -->
+
+        <!-- form 2 -->
+        <article class="forms">
+            <h2 id="form2">Образац 2</h2>
+        </article>
+        <!-- /form 2 -->
+
+        <!-- form 3 -->
+        <article class="forms">
+            <h2 id="form3">Образац 3</h2>
+        </article>
+        <!-- /form 3 -->
+
+        <!-- form 4 -->
+        <article class="forms">
+            <h2 id="form4">Образац 4</h2>
+        </article>
+        <!-- /form 4 -->
+
+    </section>
+    <!-- /content -->
+</main>
+<!-- /main -->
+
+<?php include_once './../components/footer.php'; ?>
