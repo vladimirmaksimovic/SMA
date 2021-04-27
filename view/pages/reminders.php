55d99652ffeb4d3743166f14ab8a6ac661653322
SM <svetmar.office@gmail.com>
2021-04-27 09:25:12 +0200

    adding and styling content

--- view/pages/reminders.php
+++ view/pages/reminders.php
@@ -5,8 +5,9 @@
   <aside class="aside-account-plan">
     <nav class="aside-container">
       <ul>
-        <li><a href="#ex-01">Прерачуни</a></li>
-        <li><a href="#ex-04">Књижења</a></li>
+        <li><a href="#recalculations">Прерачуни</a></li>
+        <li><a href="#non-taxable-amounts">Неопорезиви износи</a></li>
+        <li><a href="#accounting-examples">Књиговодствени примери</a></li>
         <li><a href="#"><i class="fas fa-arrow-up"></i> Врх стране</a></li>
       </ul>
     </nav>
@@ -16,9 +17,11 @@
   <!-- content  -->
   <section class="content">
 
+    <h2 id="recalculations">Примери прерачуна и обрачуна</h2>
     <article class="recalculations">
+      <!-- <h2 id="recalculations">Примери прерачуна и обрачуна</h2> -->
       <div class="recalculation">
-        <h2 id="ex-01">Прерачун норме</h2>
+        <h3 id="ex-01">Прерачун норме</h3>
         <table class="reminder-container">
           <tbody>
             <tr>
@@ -72,14 +75,14 @@
       </div>
 
       <div class="recalculation">
-        <h2 id="ex-02">Додатак на старешинство</h2>
+        <h3 id="ex-02">Додатак на старешинство</h3>
         <p>Додатак на старешинство = Основни коефицијент * 4%</p>
         <p><em>* Пример за 7. ССС</em>: 17,32 * 0,04 = 0,6928 => 0,69</p>
         <p><em>* Пример за 6. ССС</em>: 14,88 * 0,04 = 0,5952 => 0,60</p>
       </div>
 
       <div class="recalculation">
-        <h2 id="ex-03">Прерачун старешинства</h2>
+        <h3 id="ex-03">Прерачун старешинства</h3>
         <table class="reminder-container">
           <tbody>
             <tr>
@@ -133,7 +136,7 @@
       </div>
 
       <div class="recalculation">
-        <h2 id="ex-03">Обрачун превоза</h2>
+        <h3 id="ex-03">Обрачун превоза</h3>
         <p>Накнада трошкова за превоз = Број дана рада х Цена карте по дану</p>
         <table class="reminder-container">
           <tbody>
@@ -168,7 +171,12 @@
     </article>
 
     <article>
-      <h2 id="ex-04">Књижење</h2>
+      <h2 id="non-taxable-amounts">Неопорезиви износи у 2021. години</h2>
+      <img src="../../assets/images/neoporezivi-iznosi-2021-page-001.jpg" alt="Neoporezivi iznosi u 2021. godini">
+    </article>
+
+    <article>
+      <h2 id="accounting-examples">Књиговодствени примери</h2>
       <div class="first-example">
         <table class="bookkeep-reminder">
           <thead>
