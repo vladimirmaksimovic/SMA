372c3326e76eb27f5df4aedb1b17058e3778ac1b
SM <svetmar.office@gmail.com>
2021-04-28 10:58:03 +0200

    adding and styling content

--- view/pages/reminders.php
+++ view/pages/reminders.php
@@ -6,7 +6,8 @@
     <nav class="aside-container">
       <ul>
         <li><a href="#recalculations">Прерачуни</a></li>
-        <li><a href="#non-taxable-amounts">Неопорезиви износи</a></li>
+        <li><a href="#reminders-info">Неопорезиви износи у 2021.</a></li>
+        <li><a href="#reminders-info">Број радних часова у 2021.</a></li>
         <li><a href="#accounting-examples">Књиговодствени примери</a></li>
         <li><a href="#"><i class="fas fa-arrow-up"></i> Врх стране</a></li>
       </ul>
@@ -170,10 +171,30 @@
       </div>
     </article>
 
-    <article>
+    <!-- <article>
       <h2 id="non-taxable-amounts">Неопорезиви износи у 2021. години</h2>
       <img src="../../assets/images/neoporezivi-iznosi-2021-page-001.jpg" alt="Neoporezivi iznosi u 2021. godini">
     </article>
+
+    <article>
+      <h2 id="working-days">Број радних часова у 2021. години и износи минималне зараде</h2>
+      <img src="../../assets/images/broj-casova-minimalna-zarada-2021.jpg" alt="Broj radnih dana u 2021. godini">
+    </article> -->
+
+    <article>
+      <h2 id="reminders-info">Неопорезиви износи и број радних часова у 2021. години</h2>
+      <div class="reminder-info-container">
+        <div class="reminder-info">
+          <h3 id="non-taxable-amounts">Неопорезиви износи у 2021. години</h3>
+          <img src="../../assets/images/neoporezivi-iznosi-2021-page-001.jpg" alt="Neoporezivi iznosi u 2021. godini">
+        </div>
+        <div class="reminder-info">
+          <h3 id="working-days">Број радних часова у 2021. години и износи минималне зараде</h3>
+          <img src="../../assets/images/broj-casova-minimalna-zarada-2021.jpg" alt="Broj radnih dana u 2021. godini">
+        </div>
+      </div>
+    </article>
+
 
     <article>
       <h2 id="accounting-examples">Књиговодствени примери</h2>
