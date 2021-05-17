356f81daec4722be3c3fcce8ed2e00eb6e6cb4f5
VM <vmaksimovic.cr34@gmail.com>
2021-05-17 23:53:32 +0200

    adding content

--- view/pages/forms.php
+++ view/pages/forms.php
@@ -45,6 +45,32 @@ include_once '../components/header.php';
         <?php print_r($employees); ?>
       </pre>
 
+      <table>
+        <thead>
+          <tr>
+            <th>РБ</th>
+            <th>Име</th>
+            <th>Презиме</th>
+            <th>ЈРБР</th>
+            <th>ЈМБГ</th>
+            <th>Телефон</th>
+            <th>Е-мејл</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($employees as $employee) { ?>
+          <tr>
+            <td><?php echo $employee['id']; ?></td>
+            <td><?php echo $employee['name']; ?></td>
+            <td><?php echo $employee['surname']; ?></td>
+            <td><?php echo $employee['jrbr']; ?></td>
+            <td><?php echo $employee['jmbg']; ?></td>
+            <td><?php echo $employee['phone']; ?></td>
+            <td><?php echo $employee['email']; ?></td>
+          </tr>
+          <?php } ?>
+        </tbody>
+      </table>
     </article>
     <!-- /form 2 -->
 
