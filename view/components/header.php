c0a933e6b388e58c3083f1e8e06051f8bb35bcf0
SM <svetmar.office@gmail.com>
2021-08-10 12:13:52 +0200

    creating new page and image gallery

--- view/components/header.php
+++ view/components/header.php
@@ -23,9 +23,7 @@ $calculation_transportation_costs = $dao->selectCalculationTransportationCosts()
   <!-- css -->
   <link rel="stylesheet" href="./../../assets/css/style.css" />
   <!-- font awesome 5 -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-    integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
-    crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
   <title>ОШ "Светозар Марковић" Краљево</title>
 </head>
 
@@ -51,12 +49,12 @@ $calculation_transportation_costs = $dao->selectCalculationTransportationCosts()
       </ul>
 
       <ul class="sign-out">
+        <li><a href="../pages/pause.php">Пауза</a></li>
         <li class="links">
           <a href="#" class="links-drop">Линкови <i class="fas fa-sort-down"></i></а>
             <div class="links-container">
               <a href="https://crf.trezor.gov.rs/" target="_blank" title="Централни регистар фактура">ЦРФ</a>
-              <a href="https://www.croso.gov.rs/cir/index.php" target="_blank"
-                title="Централни регистар обавезног социјалног осигурања">ЦРОСО</a>
+              <a href="https://www.croso.gov.rs/cir/index.php" target="_blank" title="Централни регистар обавезног социјалног осигурања">ЦРОСО</a>
               <a href="https://www.trezor.gov.rs/src/" target="_blank">Управа за трезор</a>
               <a href="https://www.purs.gov.rs/" target="_blank">Пореска управа</a>
               <a href="https://euprava.gov.rs/" target="_blank">Е Управа</a>
---
+++ view/pages/pause.php
@@ -0,0 +1,101 @@
+<?php
+
+include_once './../components/header.php';
+include_once './../../data/data.php';
+
+$divisions_test = isset($GLOBALS["divisions_test"]) ? $GLOBALS["divisions_test"] : "";
+$school_bell = isset($GLOBALS["school_bell"]) ? $GLOBALS["school_bell"] : "";
+
+?>
+
+<main id="pause">
+  <?php include_once "../components/aside_nav.php"; ?>
+
+  <!-- content -->
+  <section class="content">
+
+    <?php include_once "../components/top_page_btn.php"; ?>
+
+    <!-- pause gallery -->
+    <h2>Strana za odmor i razonodu ...</h2>
+
+    <article class="gallery-container">
+      <img src="../../assets/img/01-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/02-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/03-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/04-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/05-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/06-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/07-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/08-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/09-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/10-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/11-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/12-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/13-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/14-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/15-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/16-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/17-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/18-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/19-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/20-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/21-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/22-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/23-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/24-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/25-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/26-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/27-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/28-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/29-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/30-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/31-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/32-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/33-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/34-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/35-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/36-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/37-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/38-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/39-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/40-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/41-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/42-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/43-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/44-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/45-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/46-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/47-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/48-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/49-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/50-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/51-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/52-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/53-flower.jpg" alt="Rose image">
+      <img src="../../assets/img/54-flower.jpg" alt="Rose image">
+    </article>
+
+
+    <!-- 
+      <table class="basic-info-table">
+        <tbody>
+          <?php foreach ($company_info as $info) { ?>
+            <tr>
+              <td><?php echo $info['info_category']; ?></td>
+              <td><?php echo $info['info_data']; ?></td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </article> -->
+
+    <!-- pause games -->
+
+    <!-- pause news links -->
+
+  </section>
+
+</main>
+
+<?php include_once '../components/footer.php'; ?>
