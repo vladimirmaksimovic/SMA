89618db1898147f98acc71dc6d02a792beb4b9b1
SM <svetmar.office@gmail.com>
2021-08-10 10:38:40 +0200

    implemnting sass styles in travel warrant section

--- view/pages/travel_warrant.php
+++ view/pages/travel_warrant.php
@@ -1,27 +1,11 @@
-<?php
+<?php include_once '../components/header.php'; ?>
 
-include_once '../components/header.php';
-
-?>
-
-<main>
+<main id="travel-warrant">
   <?php include_once "../components/aside_nav.php"; ?>
-
-  <!-- 
-  <aside>
-    nav class="aside-container">
-      <ul>
-        <li><a href="#form1">Образац 1</a></li>
-        <li><a href="#form2">Образац 2</a></li>
-        <li><a href="#form3">Образац 3</a></li>
-        <li><a href="#form4">Образац 4</a></li>
-        <li><a href="#"><i class="fas fa-arrow-up"></i> Врх стране</a></li>
-      </ul>
-      </nav>
-  </aside> -->
 
   <!-- content  -->
   <section class="content">
+
     <!-- to the top btn -->
     <?php include_once "../components/top_page_btn.php"; ?>
 
@@ -40,18 +24,23 @@ include_once '../components/header.php';
 
     <!-- output section -->
     <article class="travel-warrant-output">
+
       <!-- first page -->
       <div class="grid-container">
+
         <!-- left side -->
         <?php include_once '../components/travel-warrant/left_component.php'; ?>
+
         <!-- right side -->
         <?php include '../components/travel-warrant/right_component.php'; ?>
       </div>
 
       <!-- second page -->
       <div class="grid-container">
+
         <!-- left side -->
         <?php include_once '../components/travel-warrant/empty_component.php'; ?>
+
         <!-- right side -->
         <?php require '../components/travel-warrant/right_component.php'; ?>
 
