12e5fa01ca09630a929351d918a1f58859485db2
lion_tamer <vmaksimovic.cr34@gmail.com>
2021-08-15 22:09:55 +0200

    adding b5 cdns and importing db

--- view/components/footer.php
+++ view/components/footer.php
@@ -24,7 +24,11 @@ $footer_info = isset($GLOBALS['footer_info']) ? $GLOBALS['footer_info'] : "";
   </p>
 </footer>
 
+<!-- main js -->
 <script src="../../assets/js/main.js"></script>
+
+<!-- Bootstrap 5 JS Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 
 </body>
 
--- view/components/header.php
+++ view/components/header.php
@@ -21,9 +21,12 @@ $calculation_transportation_costs = $dao->selectCalculationTransportationCosts()
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <!-- css -->
-  <link rel="stylesheet" href="./../../assets/css/style.css" />
+  <!-- <link rel="stylesheet" href="./../../assets/css/style.css" /> -->
   <!-- font awesome 5 -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+  <!-- Bootstrap 5 CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
   <title>ОШ "Светозар Марковић" Краљево</title>
 </head>
 
