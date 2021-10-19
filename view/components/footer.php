a908160c3e2c204951ff65f593154ad827c5ec27
Vladimir <vmaksimovic.cr34@gmail.com>
2021-10-19 10:27:34 +0200

    adding data in footer from db and removing from intern array

--- view/components/footer.php
+++ view/components/footer.php
@@ -1,23 +1,77 @@
-<?php
-
-include_once '../../data/data.php';
-
-$footer_info = isset($GLOBALS['footer_info']) ? $GLOBALS['footer_info'] : "";
-
-?>
-
 <footer>
     <div class="footer-container container">
-        <?php foreach ($footer_info as $footer_div => $footer_data) { ?>
         <div class="footer-section col-3">
             <ul class="footer-grid-container">
-                <?php foreach ($footer_data as $first_info => $second_info) { ?>
-                <li><?php echo $first_info; ?></li>
-                <li><?php echo $second_info; ?></li>
-                <?php } ?>
+                <li><?php echo $company_info[1]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[1]["info_data"]; ?></li>
+                <li><?php echo $company_info[8]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[8]["info_data"]; ?></li>
+                <li><?php echo $company_info[12]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[12]["info_data"]; ?></li>
+                <li><?php echo $company_info[6]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[6]["info_data"]; ?></li>
             </ul>
         </div>
-        <?php } ?>
+        <div class="footer-section col-3">
+            <ul class="footer-grid-container">
+                <li><?php echo $company_info[2]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[2]["info_data"]; ?></li>
+                <li><?php echo $company_info[3]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[3]["info_data"]; ?></li>
+                <li><?php echo $company_info[4]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[4]["info_data"]; ?></li>
+                <li><?php echo $company_info[5]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[5]["info_data"]; ?></li>
+                <li><?php echo $company_info[7]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[7]["info_data"]; ?></li>
+            </ul>
+        </div>
+        <div class="footer-section col-3">
+            <ul class="footer-grid-container">
+                <li><?php echo $company_info[13]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[13]["info_data"]; ?></li>
+                <li><?php echo $company_info[14]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[14]["info_data"]; ?></li>
+                <li><?php echo $company_info[17]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[17]["info_data"]; ?></li>
+            </ul>
+        </div>
+        <div class="footer-section col-3">
+            <ul class="footer-grid-container">
+                <li><?php echo $company_info[19]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[19]["info_data"]; ?></li>
+                <li><?php echo $company_info[20]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[20]["info_data"]; ?></li>
+                <li><?php echo $company_info[21]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[21]["info_data"]; ?></li>
+                <li><?php echo $company_info[22]["info_category"]; ?>: </li>
+                <li><?php echo $company_info[22]["info_data"]; ?></li>
+            </ul>
+        </div>
+        <!-- <?php 
+        echo "<pre>";
+        print_r($company_info);
+        echo "</pre>";
+        
+        ?> -->
+        <!-- <div class="footer-section col-3">
+            <ul class="footer-grid-container">
+                <li><?php echo $first_info; ?></li>
+                <li><?php echo $second_info; ?></li>
+            </ul>
+        </div>
+        <div class="footer-section col-3">
+            <ul class="footer-grid-container">
+                <li><?php echo $first_info; ?></li>
+                <li><?php echo $second_info; ?></li>
+            </ul>
+        </div>
+        <div class="footer-section col-3">
+            <ul class="footer-grid-container">
+                <li><?php echo $first_info; ?></li>
+                <li><?php echo $second_info; ?></li>
+            </ul>
+        </div> -->
     </div>
     <p>
         <i class="fas fa-paint-roller"></i> by &lt;/<a href="https://github.com/vladimirmaksimovic"
